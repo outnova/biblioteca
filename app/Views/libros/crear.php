2e51ee0e9822e90ef4fd57f31b278ee53ffaef5f
outnova <bjalastre@gmail.com>
2024-12-30 23:58:42 -0400

    show old book name and "back" button added

--- app/Views/libros/crear.php
+++ app/Views/libros/crear.php
@@ -1,13 +1,12 @@
 <?=$header?>
-    Formulario de crear
     <div class="card">
         <div class="card-body">
-            <h5 class="card-title">Ingresar datos del libre</h5>
+            <h5 class="card-title">Ingresar datos del libro</h5>
             <p class="card-text">
             <form method="post" action="<?=site_url('/guardar');?>" enctype="multipart/form-data">
                 <div class="form-group">
                     <label for="nombre">Nombre</label>
-                    <input id="nombre" class="form-control" type="text" name="nombre">
+                    <input id="nombre" value="<?=old('nombre')?>" class="form-control" type="text" name="nombre">
                 </div>
 
                 <div class="form-group">
@@ -16,6 +15,7 @@
                 </div>
 
                 <button class="btn btn-success" type="submit">Guardar</button>
+                <a href="<?=base_url('listar');?>" class="btn btn-info">Cancelar</a>
             </form>
             </p>
         </div>
