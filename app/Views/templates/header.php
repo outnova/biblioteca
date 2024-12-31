c7eb3ea221df64d9d30572f096b12e83c650a26d
outnova <bjalastre@gmail.com>
2024-12-31 00:00:39 -0400

    a navbar and validation message added to header

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -7,6 +7,29 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 </head>
 <body>
-    Sistema de Libros
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand">Biblioteca</a>
+        <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div id="my-nav" class="collapse navbar-collapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="<?=base_url('listar');?>">Libros </span></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 
     <div class="container">
+
+<?php if(session('mensaje')) {?>
+    
+    <div class="alert alert-danger" role="alert">
+        <?php
+            echo session('mensaje');
+        ?>
+    </div>
+    <?php 
+    }
+?>
